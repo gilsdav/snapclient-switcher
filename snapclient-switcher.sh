831e560b93269392a0b24fb3e1e9a108e09c447e
David Gilson <davgilson@live.fr>
2017-10-12 23:21:40 +0200

    update service description

--- snapclient-switcher.sh
+++ snapclient-switcher.sh
@@ -6,8 +6,8 @@
 # Required-Stop:     $remote_fs $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: Put a short description of the service here
-# Description:       Put a long description of the service here
+# Short-Description: Snapclient host switcher
+# Description:       This service allow you to easily switch snapclient to other host
 ### END INIT INFO
 
 # Change the next 3 lines to suit where you install your script and what you want to call it
